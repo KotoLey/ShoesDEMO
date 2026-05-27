d573bf5a287474f454a01d69b0467d9710b3b71f
Влад Лобачёв <kotoley08@yahoo.com>
2026-05-27 15:11:00 +0300

    final

--- ShoesDEMO/AddEditProductWindow.xaml.cs
+++ ShoesDEMO/AddEditProductWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using ShoesDEMO.Database;
+using ShoesDEMO.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +23,7 @@ namespace ShoesDEMO
     public partial class AddEditProductWindow : Window
     {
         private ShoesDEEntitites _db = new ShoesDEEntitites();
+        private MessageHelper _mh = new MessageHelper();
         private bool _isEditing;
         private Product _product;
         public AddEditProductWindow(int? id)
@@ -98,7 +101,119 @@ namespace ShoesDEMO
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!ValidateInput())
+                return;
 
+            if (_isEditing == true)
+                UpdateProduct();
+            else
+                CreateProduct();
+        }
+
+        private void CreateProduct()
+        {
+            Product product = new Product();
+
+            string article = ProductArticle.Text;
+            string name = ProductName.Text;
+            decimal price = Convert.ToDecimal(ProductPrice.Text);
+            int discount = Convert.ToInt32(ProductDiscount.Text);
+            int amount = Convert.ToInt32(ProductAmountInStock.Text);
+            string description = ProductDescription.Text;
+            string photo = ProductPhoto.Text;
+
+            product.Article = article;
+            product.Name = name;
+            product.Price = price;
+            product.Discount = discount;
+            product.AmountInStock = amount;
+            product.Discription = description;
+
+            product.UnitId = (int)ProductUnit.SelectedValue;
+            product.ProducerId = (int)ProductProducer.SelectedValue;
+            product.ProviderId = (int)ProductProvider.SelectedValue;
+            product.CategoryId = (int)ProductCategory.SelectedValue;
+
+            try
+            {
+                _db.Product.AddOrUpdate(product);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _mh.ShowError(ex.Message);
+            }
+        }
+
+        private void UpdateProduct()
+        {
+            string article = ProductArticle.Text;
+            string name = ProductName.Text;
+            decimal price = Convert.ToDecimal(ProductPrice.Text);
+            int discount = Convert.ToInt32(ProductDiscount.Text);
+            int amount = Convert.ToInt32(ProductAmountInStock.Text);
+            string description = ProductDescription.Text;
+            string photo = ProductPhoto.Text;
+
+            _product.Article = article;
+            _product.Name = name;
+            _product.Price = price;
+            _product.Discount = discount;
+            _product.AmountInStock = amount;
+            _product.Discription = description;
+
+            _product.UnitId =(int)ProductUnit.SelectedValue;
+            _product.ProducerId = (int)ProductProducer.SelectedValue;
+            _product.ProviderId = (int)ProductProvider.SelectedValue;
+            _product.CategoryId = (int)ProductCategory.SelectedValue;
+
+            try
+            {
+                _db.Product.AddOrUpdate(_product);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _mh.ShowError(ex.Message);
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            StringBuilder errors = new StringBuilder();
+
+            string article = ProductArticle.Text;
+            string name = ProductName.Text;
+            string price = ProductPrice.Text;
+            string discount = ProductDiscount.Text;
+            string amount = ProductAmountInStock.Text;
+            string description = ProductDescription.Text;
+            string photo = ProductPhoto.Text;
+
+            if (string.IsNullOrWhiteSpace(article))
+                errors.AppendLine("Поле артикул не заполнено");
+
+            if (string.IsNullOrWhiteSpace(name))
+                errors.AppendLine("Поле наименование не заполнено");
+
+            if (string.IsNullOrWhiteSpace(price) || !decimal.TryParse(price, out decimal priceDec) || priceDec < 0)
+                errors.AppendLine("Поле цена не заполнено");
+
+            if (string.IsNullOrWhiteSpace(discount) || !int.TryParse(discount, out int discountInt) || discountInt > 100 || discountInt < 0)
+                errors.AppendLine("Поле скидка не заполнено");
+
+            if (string.IsNullOrWhiteSpace(amount) || !int.TryParse(amount, out int amountInt) || amountInt < 0)
+                errors.AppendLine("Поле количество не заполнено");
+
+            if (string.IsNullOrWhiteSpace(description))
+                errors.AppendLine("Поле описание не заполнено");
+
+            if (errors.Length > 0)
+            {
+                _mh.ShowError(errors.ToString());
+                return false;
+            }
+            return true;      
         }
     }
 }
